950c52d1a8711daabc395907b18c543f33d5781f
Crowdbotics <team@crowdbotics.com>
2023-04-23 14:14:14 +0000

    Add Copy of Blank Screen 18314

---
+++ screens/CopyofBlankScreen18314/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const CopyofBlankScreen18314 = () => {
+  return <View style={_styles.CopyofBlankScreen18314}>
+      </View>;
+};
+
+export default CopyofBlankScreen18314;
+
+const _styles = StyleSheet.create({
+  CopyofBlankScreen18314: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
