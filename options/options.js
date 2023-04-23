9d73a465b8857761b0d3a99c0092ccfd9c6023e1
Ambre SIMOND <ambresimond@hotmail.fr>
2023-04-23 16:22:00 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "game-40143",
-  url: "https://game-40143.botics.co",
-  api: "https://game-40143.botics.co/api/v1"
-}
+  "name": "game-40143",
+  "url": "https://game-40143.botics.co",
+  "api": "https://game-40143.botics.co/api/v1",
+  "initialRoute": "BlankScreen18314"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
